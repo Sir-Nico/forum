2b830bd777dfd9129497eca8a94acb36627c8252
Nico <69917886+Sir-Nico@users.noreply.github.com>
2023-05-26 00:33:56 +0200

    Removed redundant imports in app.py

--- server/app.py
+++ server/app.py
@@ -1,18 +1,16 @@
 import os
 import time
-import sqlite3
 from flask import Flask, jsonify, request, Response, send_from_directory, redirect
 from flask_cors import CORS
 from datetime import datetime, timedelta
 from flask_jwt_extended import create_access_token, JWTManager
-import sentry_sdk
-from sentry_sdk.integrations.flask import FlaskIntegration
 import database_interface as dbInterface
 from database_interface import log, Connection
 
 
 app = Flask(__name__, static_url_path='', static_folder='../client/build')
 CORS(app)
+
 
 @app.route('/<path:path>')
 def serve(path):
@@ -28,7 +26,7 @@ def defaultPage():
     return send_from_directory(app.static_folder, 'index.html')
 
 
-@app.route('/api', methods=["POST"])
+@app.route('/api', methods=['POST'])
 def api():
     return {"test": "Hello from the backend!!"}
 
